7ee2b2eb7c58a422fec0cad89293ae64b45a913a
Guhan Gnanam <guhan.gnanam@outlook.com>
2023-12-02 20:29:46 -0500

    Changes

--- Vehicle.py
+++ Vehicle.py
@@ -2,19 +2,23 @@
 
 class Vehicle:
 
-    def __init__(self, make, model, year, mileage, horsepower, body_type):
+    def __init__(self, make, model, yearFrom, yearTo, trim, horsepower, body_type):
         self.make = make
         self.model = model
-        self.year = year
-        self.mileage = mileage
-        self.horsepower = horsepower
+        self.yearFrom = (yearFrom)
+        self.yearTo = (yearTo)
+        self.trim = trim
+        self.horsepower = (horsepower)
         self.body_type = body_type
 
     def display_info(self):
-        print(f"Make: {self.make}")
-        print(f"Model: {self.model}")
-        print(f"Year: {self.year}")
-        print(f"Mileage: {self.mileage} miles")
-        print(f"Horsepower: {self.horsepower} HP")
-        print(f"Body Type: {self.body_type}")
+        return (
+            f"Make: {self.make}\n"
+            f"Model: {self.model}\n"
+            f"Year From: {self.yearFrom}\n"
+            f"Year To: {self.yearTo}\n"
+            f"Trim: {self.trim}\n"
+            f"Horsepower: {self.horsepower} HP\n"
+            f"Body Type: {self.body_type}"
+        )
 
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import tkinter as tk
 import pandas as pd
+from Vehicle import Vehicle
+
+#new_car = Vehicle("Make", "Model", "1990", "2020", "Mileage", "Horsepower", "Sedan")
 
 # Variables to track:
 # Make
@@ -13,68 +16,138 @@ import pandas as pd
 
 
 # Breakdown:
-# Two different search algorithm's narrow the large dataset into a smaller list
+# Two different search algorithm's ndatasetow the large dataset into a smaller list
 #
 
 
 
 def ternary_search(dataset, user_input):
-    # Implement ternary search algorithm
     pass
 
-def interpolation_search(dataset, user_input):
-    # Implement interpolation search algorithm
-    pass
+def interpolation_search(dataset, make, model):
+    vehicle_list = []
+    low, high = 0, len(dataset) - 1
+
+    while low <= high and dataset["Make"].iloc[low] <= make <= dataset["Make"].iloc[high]:
+        # Estimate the position based on the position of the make relative to the range
+        pos = low + int((high - low) * (dataset["Make"].tolist().index(make) - dataset["Make"].tolist().index(dataset["Make"].iloc[low])) / (dataset["Make"].tolist().index(dataset["Make"].iloc[high]) - dataset["Make"].tolist().index(dataset["Make"].iloc[low])))
+
+        if dataset["Make"].iloc[pos] == make and dataset["Model"].iloc[pos] == model:
+            # Create a Vehicle object and append it to the list
+            vehicle = Vehicle(
+                make=dataset["Make"].iloc[pos],
+                model=dataset["Model"].iloc[pos],
+                yearFrom=dataset["Year_from"].iloc[pos],
+                yearTo=dataset["Year_to"].iloc[pos],
+                trim=dataset["Trim"].iloc[pos],
+                horsepower=dataset["engine_hp"].iloc[pos],
+                body_type=dataset["Body_type"].iloc[pos]
+            )
+            vehicle_list.append(vehicle)
+
+            # Explore left side
+            left = pos - 1
+            while left >= low and dataset["Make"].iloc[left] == make and dataset["Model"].iloc[left] == model:
+                vehicle_left = Vehicle(
+                    make=dataset["Make"].iloc[left],
+                    model=dataset["Model"].iloc[left],
+                    yearFrom=dataset["Year_from"].iloc[left],
+                    yearTo=dataset["Year_to"].iloc[left],
+                    trim=dataset["Trim"].iloc[left],
+                    horsepower=dataset["engine_hp"].iloc[left],
+                    body_type=dataset["Body_type"].iloc[left]
+                )
+                vehicle_list.append(vehicle_left)
+                left -= 1
+
+            # Explore right side
+            right = pos + 1
+            while right <= high and dataset["Make"].iloc[right] == make and dataset["Model"].iloc[right] == model:
+                vehicle_right = Vehicle(
+                    make=dataset["Make"].iloc[right],
+                    model=dataset["Model"].iloc[right],
+                    yearFrom=dataset["Year_from"].iloc[right],
+                    yearTo=dataset["Year_to"].iloc[right],
+                    trim=dataset["Trim"].iloc[right],
+                    horsepower=dataset["engine_hp"].iloc[right],
+                    body_type=dataset["Body_type"].iloc[right]
+                )
+                vehicle_list.append(vehicle_right)
+                right += 1
+
+            return vehicle_list  # All occurrences found
+
+        elif dataset["Make"].iloc[pos] < make or (dataset["Make"].iloc[pos] == make and dataset["Model"].iloc[pos] < model):
+            low = pos + 1
+        else:
+            high = pos - 1
+
+    return None  # Make and model not found
+
+def linear_scrub(vehicles, user_input):
+    filtered_vehicles = []
+
+    for vehicle in vehicles:
+        #print(vehicle.trim)
+        # Check if the vehicle matches the user's input criteria
+        if (
+            (user_input["year"] == "" or (user_input["year"] == "" or int(user_input["year"]) >= vehicle.yearFrom and int(user_input["year"]) <= vehicle.yearTo)) and
+            (user_input["trim"] == "" or (user_input["trim"] == "" or user_input["trim"] == vehicle.trim)) and
+            (user_input["horsepower"] == "" or (user_input["horsepower"] != "" and int(user_input["horsepower"]) == vehicle.horsepower)) and
+            (user_input["body type"] == "" or user_input["body type"] == vehicle.body_type)
+            # Add other criteria as needed
+        ):
+            filtered_vehicles.append(vehicle)
+
+    return filtered_vehicles
 
 
-def search_cars(dataset, user_input):
-    # Search algorithm to find cars that match the user input
-    matching_cars = []
-
-    for index, row in dataset.iterrows():
-        match = True
-
-        # Check each attribute for a match
-        for key, value in user_input.items():
-            if value and str(row[key]).lower() != value.lower():  # Ignore blank values
-                match = False
-                break
-
-        if match:
-            matching_cars.append(row)
-            if len(matching_cars) == 5:
-                break  # Stop when 5 matches are found
-
-    return matching_cars
 
 
-def search_button_click(year_entry, model_entry, mileage_entry, horsepower_entry, body_type_var, algorithm_var,  result_label, dataset):
-    '''
+def search_button_click(year_entry, model_entry, make_entry, trim_entry, hp_entry, body_type_var, algorithm_var, result_label):
     user_input = {
         "year": year_entry.get(),
+        "make": make_entry.get(),
         "model": model_entry.get(),
-        "mileage": mileage_entry.get(),
-        "horsepower": horsepower_entry.get(),
+        "trim": trim_entry.get(),
+        "horsepower": hp_entry.get(),
         "body type": body_type_var.get(),
         # Add other specifications
     }
-
+    dataset = pd.read_csv("cars.csv", low_memory=False)
     # Determine the selected searching algorithm
     selected_algorithm = algorithm_var.get()
-    #print("Called")
-    result_text = "Name"  # Initialize result_text with a default value
+
+    if selected_algorithm == "Interpolation Search":
+        # Perform interpolation search to find cars of the specified make and model
+        interpolated_cars = interpolation_search(dataset, user_input["make"], user_input["model"])
+
+        if not interpolated_cars:
+            result_label.config(text="No cars found for the given make and model.")
+            return
+
+        # Perform linear search within the interpolated cars for matching attributes
+        result = linear_scrub(interpolated_cars, user_input)
+        print(result)
+        if not result:
+            result_label.config(text="No matching cars found.")
+        else:
+            result_text = [car.display_info() for car in result]
+            result_label.config(text="\n".join(result_text))
+    else:
+        result_label.config(text="Invalid algorithm selected.")
 
     if selected_algorithm == "Ternary Search":
-        result = ternary_search(dataset, user_input)
-    elif selected_algorithm == "Interpolation Search":
-        result = interpolation_search(dataset, user_input)
-    else:
-        result = "Invalid algorithm selected."
+        pass
+def print_vehicle_data(vehicles):
+    for vehicle in vehicles:
+        print(vehicle.display_info())
+        print("=" * 30)  # Separator for better readability
 
-    result_text = result  # Update result_text with the actual result
+def print_test(results):
+    for car in results:
+        print(car.trim)
 
-    result_label.config(text=result_text)  # Update the text of the result_label
-    '''
 
 def main():
     # Load the dataset
@@ -83,7 +156,7 @@ def main():
     # Create the main application window
     app = tk.Tk()
     app.title("DSA Project 3")
-    app.geometry("500x500")
+    app.geometry("1000x1000")
 
     title_label = tk.Label(app, text="Enter Information on Desired Vehicle")
     title_label.grid(row=0, column=1, padx=10, pady=10)
@@ -108,11 +181,11 @@ def main():
     model_entry = tk.Entry(app)
     model_entry.grid(row=3, column=1, padx=10, pady=10)
 
-    # Mileage
-    mileage_label = tk.Label(app, text="Mileage:")
-    mileage_label.grid(row=4, column=0, padx=10, pady=10)
-    mileage_entry = tk.Entry(app)
-    mileage_entry.grid(row=4, column=1, padx=10, pady=10)
+    # Trim
+    trim_label = tk.Label(app, text="Trim:")
+    trim_label.grid(row=4, column=0, padx=10, pady=10)
+    trim_entry = tk.Entry(app)
+    trim_entry.grid(row=4, column=1, padx=10, pady=10)
 
     # Horsepower
     hp_label = tk.Label(app, text="Horsepower:")
@@ -138,12 +211,30 @@ def main():
     algorithm_menu.grid(row=7, column=1, columnspan=2, pady=10)
 
     # Create and place the Search button
-    search_button = tk.Button(app, text="Search", command=lambda: search_button_click(year_entry, model_entry, mileage_entry, hp_entry, body_type_var, algorithm_var, result_label, dataset))
+    search_button = tk.Button(app, text="Search", command=lambda: search_button_click(year_entry, model_entry, make_entry, trim_entry, hp_entry, body_type_var, algorithm_var, result_label))
     search_button.grid(row=8, column=0, columnspan=2, pady=10)
 
-    # Define the result label
-    result_label = tk.Label(app, text="Blank")
-    result_label.grid(row=9, column=0, columnspan=2, pady=10)
+    result_label = tk.Label(app, text="")
+    result_label.grid(row=9, column=0, padx=10, pady=10)
+
+
+    user_input = {
+        "year": "1997",
+        "make": "AC",
+        "model": "ACE",
+        "trim": "3.5 MT",
+        "horsepower": "354",
+        "body type": "Cabriolet",
+        # Add other specifications
+    }
+
+    make = "AC"
+    model ="ACE"
+    results = interpolation_search(dataset, make, model)
+    print_vehicle_data(results)
+    final = linear_scrub(results, user_input)
+    print_test(final)
+
 
 
 
